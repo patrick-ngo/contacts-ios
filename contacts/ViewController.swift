de5a9972e381c486a2d1450ac6d9dbdead2e5730
Patrick Ngo <patrick.ngo@hotmail.com>
2018-06-19 21:11:47 +0800

    Initial empty project

---
+++ contacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  contacts
+//
+//  Created by Patrick Ngo on 2018-06-19.
+//  Copyright © 2018 Patrick Ngo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
